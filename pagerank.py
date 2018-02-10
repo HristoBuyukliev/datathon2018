9e8ca8d4a0e41cc93f8de69018318a9a66586d99
Martin Boyanov <mboyanov@gmail.com>
2018-02-10 14:38:02 +0200

    Add weighted option for pagerank

--- pagerank.py
+++ pagerank.py
@@ -2,22 +2,29 @@ import collections
 import operator
 from data import read_data
 from pandas import DataFrame
+from link import Link
 
+def getWeights(outnodes):
+    weights = {}
+    for k, links in outnodes.items():
+        normalization = sum(l.strength for l in links)
+        weights[k] = [l.strength/normalization for l in links]
+    return weights
 
-def main(graphData: DataFrame, alpha=0.8):
+def main(graphData: DataFrame, alpha=0.8, use_weights=False):
     seen = set()
-    outnodes = collections.defaultdict(lambda: set())
-    innodes = collections.defaultdict(lambda: set())
+    outnodes = collections.defaultdict(lambda: [])
     for i, r in graphData.iterrows():
         user_from = i[0]
         user_to = i[1]
-        outnodes[user_from].add(user_to)
-        innodes[user_to].add(user_from)
+        link = Link(user_from, user_to, r['strength'])
+        outnodes[user_from].append(link)
         seen.add(user_from)
         seen.add(user_to)
     print("Graph created")
     currentpageranks = collections.defaultdict(lambda: 1.0 / len(seen))
     newpageranks = collections.defaultdict()
+    weights = getWeights(outnodes)
     for iteration in range(100):
         C = 0
         print('seen', len(seen))
@@ -29,10 +36,13 @@ def main(graphData: DataFrame, alpha=0.8):
         print(C)
         for r in seen:
             newpageranks[r] = (1 - alpha + alpha * C) / len(seen)
-        for r in outnodes:
-            q = outnodes[r]
-            for entry in q:
-                newpageranks[entry] += alpha * currentpageranks[r] / len(q)
+        for r, outgoing in outnodes.items():
+            current_weights = weights[r]
+            for i, link in enumerate(outgoing):
+                weight = 1/ len(outgoing)
+                if use_weights:
+                    weight = current_weights[i]
+                newpageranks[link.target] += alpha * currentpageranks[r] * weight
         delta = sum(abs(currentpageranks[x]- newpageranks[x]) for x in newpageranks)
         print("Delta", delta)
         currentpageranks = newpageranks
@@ -41,10 +51,10 @@ def main(graphData: DataFrame, alpha=0.8):
 
 
 graphData = read_data()
-results = main(graphData)
+results = main(graphData, use_weights=True)
 pageranks = sorted(results.items(), key=operator.itemgetter(1), reverse=True)
 
-with open('pr.txt', 'w') as out:
+with open('pr-weighted.txt', 'w') as out:
     for pagerank in pageranks:
         out.write(str(round(pagerank[1], 8)) + " " + str(pagerank[0]))
         out.write('\n')
